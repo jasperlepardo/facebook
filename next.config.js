3a36cc115848f95d2821b91d1789711a32ca9629
gzcdmmwywv-pixel <gzcdmmwywv@privaterelay.appleid.com>
2026-07-22 14:12:28 +0800

    Disable TLS verification to fix MongoDB SSL on Vercel

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,3 +1,4 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 import { MongoClient, MongoClientOptions } from 'mongodb'
 import { NextRequest, NextResponse } from 'next/server'
 
